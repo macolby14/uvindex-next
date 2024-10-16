4bcf0bc93b67cc8ba8c17e6536008fcb5f83a186
Mark Colby <macolby14@gmail.com>
2024-10-16 15:18:26 -0400

    Make proxy env variable check more explicit

--- src/app/api/util.ts
+++ src/app/api/util.ts
@@ -1,7 +1,7 @@
 import { setGlobalDispatcher, ProxyAgent } from "undici";
 
 export function useProxyIfSet() {
-  if (process.env.HTTPS_PROXY) {
+  if (process.env.HTTPS_PROXY != null && process.env.HTTPS_PROXY !== "") {
     console.log("Using HTTPS_PROXY=", process.env.HTTPS_PROXY);
     // Corporate proxy uses CA not in undici's certificate store
     process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
