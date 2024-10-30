b4f825869ec62fc5afc7ecb758bd6662978c1421
Mark Colby <macolby14@gmail.com>
2024-10-30 14:14:55 -0400

    Add dockerfile for self-host deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "standalone", // https://github.com/vercel/next.js/tree/canary/examples/with-docker
+};
 
 export default nextConfig;
